b3b9c505e1944af8d24b9a4170e71cc2f224c6e6
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-26 16:49:11 +0200

    Migrate template UI to Freetime-Core 2.0.0

--- app/src/main/java/com/freetime/template/MainActivity.kt
+++ app/src/main/java/com/freetime/template/MainActivity.kt
@@ -4,31 +4,50 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
-import com.freetime.design.FreetimeScaffold
-import com.freetime.design.FreetimeText
-import com.freetime.design.FreetimeTheme
+import androidx.compose.ui.unit.dp
+import com.freetime.design.AppTheme
+import com.freetime.design.ThemeMode
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FreetimeTheme {
-                FreetimeScaffold(modifier = Modifier.fillMaxSize()) {
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier
-                            .align(Alignment.Center)
-                            .padding(24.dp)
-                    )
-                }
+            TemplateApp()
+        }
+    }
+}
+
+@Composable
+private fun TemplateApp() {
+    AppTheme(
+        themeMode = ThemeMode.AUTO_TIME,
+        liquidGlassEnabled = true,
+    ) {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            containerColor = MaterialTheme.colorScheme.background,
+        ) { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                contentAlignment = Alignment.Center,
+            ) {
+                Greeting(
+                    name = "Android",
+                    modifier = Modifier.padding(24.dp),
+                )
             }
         }
     }
@@ -36,16 +55,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    FreetimeText(
+    Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        style = MaterialTheme.typography.headlineMedium,
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    FreetimeTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
